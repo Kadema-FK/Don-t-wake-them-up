3f7757bd822413ddcae700ff6582f94c93cc048b
Kadema-FK <102257975+Kadema-FK@users.noreply.github.com>
2023-09-09 18:03:09 +0200

    Timers

--- Scripts/don't wake her up.gd
+++ Scripts/don't wake her up.gd
@@ -1,18 +1,39 @@
 extends Node2D
 
-var clock;
-const hour = 9000;
 var day = 6;
-
+var eventTime = [[5,8], [4,7], [3,6]]
+var hour = 0;
+const sInHour = 20;
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	clock=0;
+func _ready():	
+	resetEventTimer(eventTime[0][0], eventTime[0][1]);
+	$GameTimer.start(sInHour);
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	clock+=1;
-	get_node("Test_Clock").text = str(clock) + " - " + str(clock/hour) + ":" + str(clock%hour);
 	pass
+
+func resetEventTimer(start, end):
+	var rng = RandomNumberGenerator.new();
+	var time = rng.randf_range(start, end);
+	print ("New time: " + str(time));
+	$EventTimer.start(time);
+
+func _on_game_timer_timeout():
+	hour += 1;
+	pass # Replace with function body.
+
+
+func _on_event_timer_timeout():
+	var maks_array = eventTime.size()-1;
+	#print("Maks "+str(maks_array))
+	#print ("Timeout. Game time: "+ str(hour)  + " hour: " + str($GameTimer.time_left) + "s")
+	$Obstacle.start();
+	if hour < maks_array:
+		resetEventTimer(eventTime[hour][0], eventTime[hour][1]);
+	else:
+		resetEventTimer(eventTime[maks_array][0], eventTime[maks_array][1]);
+	pass # Replace with function body.
