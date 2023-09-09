5ea28a7f7828e23e3875f0197a71d659c77d38c9
Kadema-FK <102257975+Kadema-FK@users.noreply.github.com>
2023-09-10 01:14:16 +0200

    Cat

--- Scripts/cat_obs.gd
+++ Scripts/cat_obs.gd
@@ -1,8 +1,19 @@
 extends Node
+var mouse_in = false
+var state = 0
+var noise_level = 0
 
+#var isDoingMischief: bool = false;
+const time = 5;
+
+enum {SLEEP, LOOKING, TOUCHING, SCARED}
+
+@onready var catAnimatedSprite = $Sprite2D/CatAnimatedSprite;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	catAnimatedSprite.play("sleep");
+	#start();
 	pass # Replace with function body.
 
 
@@ -12,4 +23,42 @@ func _process(delta):
 
 func start():
 	print("Cat starts");
+	#isDoingMischief = true;
+	state = LOOKING;
+	catAnimatedSprite.play("pac2");
+	$MischiefTimer.start(time);
 	pass
+
+
+func _on_mischief_timer_timeout():
+	if state == LOOKING:
+		catAnimatedSprite.play("pac3");
+		state = TOUCHING
+		$MischiefTimer.start(time);
+	elif state == TOUCHING:
+		noise_level = 10;
+		state = SCARED;
+		$MischiefTimer.start(time);
+	elif state == SCARED:
+		state = SLEEP;
+		noise_level = 0;
+		catAnimatedSprite.play("sleep");
+		
+	pass # Replace with function body.
+
+func _input(event):
+	#print("_input")
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and mouse_in == true:
+			print("State: ", state)
+			if state >= LOOKING:
+				state = SLEEP;
+				catAnimatedSprite.play("sleep");
+				noise_level = 0;
+				$MischiefTimer.stop()
+
+func _on_area_2d_mouse_entered():
+	mouse_in = true;
+
+func _on_area_2d_mouse_exited():
+	mouse_in = false;
