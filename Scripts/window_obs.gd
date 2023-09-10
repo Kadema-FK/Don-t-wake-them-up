9963dd30eed5f65fff62d443e49ea4ad4f476e60
TPDG <2kamcio1@gmail.com>
2023-09-11 00:24:15 +0200

    Window filnal,

--- Scripts/window_obs.gd
+++ Scripts/window_obs.gd
@@ -4,6 +4,7 @@ var state = 4
 var open = true
 var noise_level = 0
 var hot_level = 0
+var hot_noise = 0
 @export var catSelected = false;
 
 # Called when the node enters the scene tree for the first time.
@@ -25,13 +26,20 @@ func _input(event):
 					$window_sound.play()
 					$AudioStreamPlayer.volume_db = -20
 					noise_level = 0
+					if state == 2:
+						$AnimatedSprite2D.play("closed_wewo")
+					if state == 3:
+						$AnimatedSprite2D.play("closed_big_wewo")
 				elif open == false:
 					open = true
 					$window_sound.play()
 					$AudioStreamPlayer.volume_db = 0
 					$AnimatedSprite2D.play("open")
+					if state == 2:
+						$AnimatedSprite2D.play("open_wewo")
 					if state == 3:
 						noise_level = 10
+						$AnimatedSprite2D.play("open_big_wewo")
 
 func _on_area_2d_mouse_entered():
 	mouse_in = true;
@@ -48,7 +56,7 @@ func start():
 
 #return real noise level
 func get_noise_level():
-	return noise_level;
+	return noise_level + hot_noise;
 	
 func is_baby_hot():
 	pass
@@ -57,14 +65,26 @@ func is_baby_hot():
 func _on_timer_timeout():
 	state += 1
 	if state == 2:
-		$Weewoo.visible = true
+		if open == true:
+			$AnimatedSprite2D.play("open_wewo")
+		if open == false:
+			$AnimatedSprite2D.play("closed_wewo")
 	if state == 3 and open == true:
 		$AudioStreamPlayer.play()
+		if open == true:
+			$AnimatedSprite2D.play("open_big_wewo")
+		if open == false:
+			$AnimatedSprite2D.play("closed_big_wewo")
 		noise_level = 10
 	if state >= 4:
 		noise_level = 0
 		$AudioStreamPlayer.stop()
-		$Weewoo.visible = false
+		
+		if open == true:
+			$AnimatedSprite2D.play("open")
+		if open == false:
+			$AnimatedSprite2D.play("closed")
+			
 		$Timer.stop()
 		
 	print("Window noise: ", noise_level)
@@ -82,7 +102,7 @@ func _on_temp_timer_timeout():
 		$Termometer.play("normal")
 	if hot_level == 2:
 		$Termometer.play("warm")
-		noise_level = 0
+		hot_noise = 0
 	if hot_level == 3:
 		$Termometer.play("hot")
-		noise_level = 10
+		hot_noise = 10
